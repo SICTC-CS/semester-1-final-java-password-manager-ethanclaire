0a05ade1d724cb5e062f3fbe96ec7c53911f0147
B2 <claire.spindler@stu.evsck12.com>
2024-12-11 13:57:53 -0600

    updated

--- src/MainsPassword.java
+++ src/MainsPassword.java
@@ -367,15 +367,9 @@ public class MainsPassword {
                             System.err.println("An error with name");
                         }
 
-<<<<<<< HEAD
-                        System.out.println("\n\tWould you like to \n\t\t1. Enter your own password\n\t\t2.Generate a password");
-                    System.out.println("\nEnter choice (1-2)");
-                    int ychoice = 2;
-=======
-                        System.out.println("\n\tWould you like to \n\t\t1. Enter your own password\n\t\t2. Generate a password\n\t\t3. Back");
+                        System.out.println("\n\tWould you like to \n\t\t1. Enter your own password\n\t\t2. Generate a password");
                     System.out.println("\nEnter choice (1-3)");
                     int ychoice = 0;
->>>>>>> 5f76fd71446f78ad61d09eff2aa67bc034ef83b2
                     try {
                         ychoice = scanner.nextInt();
                     } catch (Exception e) {
